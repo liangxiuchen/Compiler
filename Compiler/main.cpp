0b36acf421958cd5b944beb7c460d7e953ca07c9
carroll chen <carroll.chen@zoom.us>
2020-04-18 20:08:38 +0800

    Initial Commit

---
+++ Compiler/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Compiler
+//
+//  Created by carroll chen on 2020/4/18.
+//  Copyright © 2020 carroll chen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
